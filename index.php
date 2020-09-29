69a7c1eaae988e3263f1ee5c0e588fe6b4680e1f
JanSlechta360 <60642873+JanSlechta360@users.noreply.github.com>
2020-09-29 19:52:16 +0200

    Update index.php
    im trying to get it

--- index.php
+++ index.php
@@ -1,42 +1,63 @@
+
 <?php
-    $auto = filter_input(INPUT_POST, "auta");
-    $spz = filter_input(INPUT_POST, "spz");
-    $rok = filter_input(INPUT_POST, "number");
-    $submit = filter_input(INPUT_POST, "submit");
-    $types = array("osobní","nákladní","autobus","karavan","trolejbus");
+
+$auto = filter_input(INPUT_POST, 'auta');
+$types = array('Osobní', 'Kupé', 'Dodávka', 'Pick-up', 'Kabriolet');
+$year = filter_input(INPUT_POST, 'year');
+$spz = filter_input(INPUT_POST, 'spz');
+$submit = filter_input(INPUT_POST, 'submit');
 
 ?>
 <!DOCTYPE html>
-              <html lang="en">
-                             <head>
-                                  <meta charset="UTF-8">
-                                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                                                                                                       <title>Document</title>
-
-                             </head>
-                                <body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>idk</title>
+</head>
+      <body>
 <?php
-if (isset ($submit)) { ?>
-    Typ:<?= $auto ?> <br>
-    SPZ:<?= $spz ?>
-    Rok výroby<?= $rok ?>
+        if (isset($submit)) { ?>
+                                Typ: <?= $auto ?><br>
+                                SPZ: <?= $spz ?><br>
+                                Rok: <?= $year ?> 
+<?php
+
+if ($year >= 2010) { ?>
+    Moderní
     <?php
-}    else {  ?>   
-              <form action="index.php" method="post">
-                Typ vozidla:<select name="auta" id="auta">
-                    <?php foreach ($types as $type) { ?>
-                        <option value="<?= $type ?>"><?= $type ?> </option>        
-<?php 
-    } ?>
-      
-                            </select> 
-     
-<br> SPZ     <input type="text" name="spz"><br>
-<br> Rok výroby   <input type="rok" name="rok"> <br>
-if  
-<input type="submit" name="submit" value="odeslat">
-        
-              </form>
+
+                } elseif ($year >= 1991) { ?>
+                Ojeté
+                <?php
+
+                    } else { ?>
+                    Veterán
+                    <?php
+}
+
+}  else { ?>
+
+    <form action="index.php" method="post">
+    
+    Typ vozidla: <select name="auta" id="auta">
+        <?php foreach ($types as $type) { ?>
+                 <option value="<?= $type ?>"><?= $type ?></option>
+        <?php
+        } ?>    
+                </select>
+
+                <br>
+
+    SPZ:         <input type="text" name="spz"><br>
+
+    Rok výroby:  <input type="number" name="year" id="year" max="2020" min="1886"><br>
+                 
+                 <input type="submit" name="submit" value="Odeslat">
+</form>
+
 <?php
 } ?>
-                                </body>
+
+</body>
+</html>
